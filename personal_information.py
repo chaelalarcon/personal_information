7a1388a486162040b51b4d11085c41f83bd188b8
unknown <johnmichaelalarcon2006@gmail.com>
2024-11-19 14:44:58 +0700

    promt the user for personal information

---
+++ personal_information.py
@@ -0,0 +1,6 @@
+full_name = input("Enter your full name: ")
+age = int(input("Enter your age: "))
+birthday = int(input("Enter your birthday: "))
+year_level = input("Enter your year level (in words): ")
+program = input("Enter your program: ")
+section = int(input("Enter your section: "))
