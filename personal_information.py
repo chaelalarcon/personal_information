26405c8b9977cdeaa6233de12642d99b833133c8
unknown <johnmichaelalarcon2006@gmail.com>
2024-11-19 15:54:02 +0700

    applying while loop and handling errors

--- personal_information.py
+++ personal_information.py
@@ -1,6 +1,14 @@
-full_name = input("Enter your full name: ")
-age = int(input("Enter your age: "))
-birthday = int(input("Enter your birthday: "))
-year_level = input("Enter your year level (in words): ")
-program = input("Enter your program: ")
-section = int(input("Enter your section: "))
+def collect_information():
+    # Ask the user for their personal information
+    full_name = input("Enter your full name: ")
+    age = int(input("Enter your age: "))
+    birthday = int(input("Enter your birthday (MM-DD-YYYY): "))
+    year_level = input("Enter your year level (in words): ")
+    program = input("Enter your program: ")
+    section = int(input("Enter your section: "))
+
+    parts = birthday.split('-')
+    if len(birthday) != 3:
+        print ("Error: Follow the date format (MM-DD-YYYY)")
+        return None
+    
